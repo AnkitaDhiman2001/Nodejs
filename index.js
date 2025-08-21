d53c26c61d9a461acb0287b3794bb53ead60b56f
AnkitaDhiman2001 <161723895+AnkitaDhiman2001@users.noreply.github.com>
2025-08-21 07:04:13 +0000

    Added port

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ app.get("/", (req, res) => {
   res.send("Hello World from Replit!");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
